23eb3cab321bb173852895aabfc08d75143553ae
Renford <chaofree3088@sina.com>
2018-02-02 10:57:15 +0800

    二叉树

--- code/search/search_tree.js
+++ code/search/search_tree.js
@@ -28,22 +28,18 @@ class BinaryTree {
             if (data < tree.data) {
                 if (tree.left == null) {
                     tree.left = node;
-                    return tree;
                 } else if (data > tree.left.data) {
                     node.left = tree.left
                     tree.left = node
-                    return tree;
                 } else {
                     return insertBST(tree.left, data)
                 }
             } else {
                 if (tree.right == null) {
                     tree.right = node;
-                    return tree;
                 } else if (data < tree.right.data) {
                     node.right = tree.right
                     tree.right = node
-                    return tree;
                 } else {
                     return insertBST(tree.right, data)
                 }
