946f493db991c19535cda1fd24178dc003d34e97
Renford <chaofree3088@sina.com>
2018-10-24 10:34:00 +0800

    [Add] 最小栈

---
+++ code/test/test_struct.js
@@ -0,0 +1,22 @@
+const struct = require('../struct')
+
+var obj = new struct.MinStack()
+obj.push(2147483646)
+obj.push(2147483646)
+obj.push(2147483647)
+console.log(obj.top())
+obj.pop()
+console.log(obj)
+
+console.log(obj.getMin())
+obj.pop()
+console.log(obj.getMin())
+obj.pop()
+obj.push(2147483647)
+
+console.log(obj.top())
+console.log(obj.getMin())
+obj.push(-2147483648)
+console.log(obj.top())
+obj.pop()
+console.log(obj.getMin())
--- doc/alg_compression.md
+++ doc/alg_compression.md
@@ -1,5 +1,9 @@
 ## 哈夫曼编码（Huffman Coding）
 
+霍夫曼编码（Huffman Coding）是一种用于无损数据压缩的熵编码（权编码）算法。又称最优二叉树，是一种带权路径长度最短的二叉树。其中，带权路径长度，就是树中所有的叶结点的权值乘上其到根结点的路径长度
 <!-- https://baijiahao.baidu.com/s?id=1565726982842058&wfr=spider&for=pc -->
 
 ## 
+
+<!-- protocol buffer -->
+<!-- http://blog.csdn.net/carson_ho/article/details/70568606 -->
